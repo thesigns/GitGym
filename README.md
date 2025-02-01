5b0023d24781514c92c2f18d8fad71a942782b2f
Jakub <jwf.adamczyk@gmail.com>
2025-02-01 20:29:31 +0100

    add simple svg example

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# GitGym
+# GitGym 0.1
 
 GitGym is a simple Git training repository.
+
+Note: you can open src/index.svg file in your browser.
---
+++ src/main.js
@@ -0,0 +1,23 @@
+
+
+window.addEventListener('resize', OnWindowResized);
+OnWindowResized();
+
+
+function OnWindowResized() {
+    
+    let svgRoot = document.documentElement;
+    let background = document.getElementById("background");
+    let middlecirc = document.getElementById("middlecirc");
+    let circlabel = document.getElementById("circlabel");
+
+    let width = svgRoot.clientWidth;
+    let height = svgRoot.clientHeight;
+
+    background.style.width = width;
+    background.style.height = height;
+    middlecirc.style.cx = width / 2;
+    middlecirc.style.cy = height / 2;
+    circlabel.setAttribute('x', width / 2);
+    circlabel.setAttribute('y', height / 2 + 25);
+}
